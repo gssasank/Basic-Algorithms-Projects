0fa6cee1e3d9e01b9937c86535b37c2b862eebff
agustuz <gssasank@gmail.com>
2020-07-14 11:42:11 +0530

    Updated according to reviewer's suggestions V2.0

---
+++ Autocomplete-with-Tries/problem_5.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Building a Trie in Python
+# 
+# Before we start let us reiterate the key components of a Trie or Prefix Tree. A trie is a tree-like data structure that stores a dynamic set of strings. Tries are commonly used to facilitate operations like predictive text or autocomplete features on mobile phones or web search.
+# 
+# Before we move into the autocomplete function we need to create a working trie for storing strings.  We will create two classes:
+# * A `Trie` class that contains the root node (empty string)
+# * A `TrieNode` class that exposes the general functionality of the Trie, like inserting a word or finding the node which represents a prefix.
+# 
+# Give it a try by implementing the `TrieNode` and `Trie` classes below!
+
+# In[38]:
+
+
+# Used the template given in the practice jupyter notebook
+class TrieNode(object):
+    def __init__(self):
+        self.is_word = False
+        self.children = {}
+
+    def insert(self, char):
+        # Add a child node in this Trie
+        self.children[char] = TrieNode()
+
+
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        Add `word` to trie
+        """
+        current_node = self.root
+
+        current_node = self.root
+
+        for char in word:
+            if char not in current_node.children:
+                current_node.children[char] = TrieNode()
+
+            current_node = current_node.children[char]
+
+        current_node.is_word = True
+
+    def find(self, word):
+        """
+        Check if word exists in trie, if present return it
+        """
+        current_node = self.root
+
+        for char in word:
+            if char not in current_node.children:
+                return None
+
+            current_node = current_node.children[char]
+
+        return current_node
+
+
+# # Finding Suffixes
+# 
+# Now that we have a functioning Trie, we need to add the ability to list suffixes to implement our autocomplete feature.  To do that, we need to implement a new function on the `TrieNode` object that will return all complete word suffixes that exist below it in the trie.  For example, if our Trie contains the words `["fun", "function", "factory"]` and we ask for suffixes from the `f` node, we would expect to receive `["un", "unction", "actory"]` back from `node.suffixes()`.
+# 
+# Using the code you wrote for the `TrieNode` above, try to add the suffixes function below. (Hint: recurse down the trie, collecting suffixes as you go.)
+
+# In[40]:
+
+
+class TrieNode(object):
+    def __init__(self):
+        self.is_word = False
+        self.children = {}
+
+    def insert(self, char):
+        self.children[char] = TrieNode()
+
+    def suffixes(self, suffix=''):
+        if self.children is not None:
+            for char, node in self.children.items():
+                if node.is_word:
+                    autocomplete.append(suffix + char)
+                node.suffixes(suffix + char)
+        return list(autocomplete)
+
+
+# # Testing it all out
+# 
+# Run the following code to add some words to your trie and then use the interactive search box to see what your code returns.
+
+# In[41]:
+
+
+autocomplete = list()
+MyTrie = Trie()
+wordList = [
+    "ant", "anthology", "antagonist", "antonym",
+    "fun", "function", "factory",
+    "trie", "trigger", "trigonometry", "tripod"
+]
+for word in wordList:
+    MyTrie.insert(word)
+
+# In[42]:
+
+
+from ipywidgets import widgets
+from IPython.display import display
+from ipywidgets import interact
+
+
+def f(prefix):
+    if prefix != '':
+        prefixNode = MyTrie.find(prefix)
+        if prefixNode:
+            print('\n'.join(prefixNode.suffixes()))
+        else:
+            print(prefix + " not found")
+    else:
+        print('')
+
+
+interact(f, prefix='t');
